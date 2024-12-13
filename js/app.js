10aaa415064885d552f5a7e60c0c50363eb278a2
djembeleza <kaola730@gmail.com>
2024-12-13 21:27:59 +0200

    Added marker

--- js/app.js
+++ js/app.js
@@ -1,36 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function geoFindMe() {
-        const status = document.querySelector("#status");
-        const mapLink = document.querySelector("#map-link");
 
-        mapLink.href = "";
-        mapLink.textContent = "";
 
-        function success(position) {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
+    let map = L.map("map").fitWorld();
 
-            status.textContent = "";
-            mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-            mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-        }
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
 
-        function error() {
-            status.textContent = "Unable to retrieve your location";
-        }
+    map.locate({setView: true, maxZoom: 16})
 
-        if (!navigator.geolocation) {
-            status.textContent = "Geolocation is not supported by your browser";
-        } else {
-            status.textContent = "Locating…";
-            navigator.geolocation.watchPosition(
-                success,
-                error,
-                { enableHighAccuracy: true}
-            );
-        }
+
+    function onLocationFound(e) {
+        let radius = e.accuracy;
+
+        L.marker(e.latlng).addTo(map)
+        .bindPopup(`You are within ${radius} meters from this point`)
+        .openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
     }
 
-    document.querySelector("#find-me").addEventListener("click", geoFindMe);
+    map.on("locationfound", onLocationFound);
 
+
+    map.on("locationerror", (e) => {
+        alert(e.message);
+    })
+    
 })
